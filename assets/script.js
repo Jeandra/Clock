5a866075ceb9e56f52365406f0df44ef0d1f36b4
Jeandra <jeandra8789@gmail.com>
2023-04-19 22:24:07 -0300

    Update script.js

--- assets/script.js
+++ assets/script.js
@@ -6,18 +6,14 @@ function clock() {
     var h = new Date().getHours();
     var m = new Date().getMinutes();
     var s = new Date().getSeconds();
-    var am = "AM";
-    if(h > 12) {
-        h = h - 12;
-        var am = "PM" 
-    } 
+    
     h = (h < 10) ? "0" + h:h
     m = (m < 10) ? "0" + m:m
     s = (s < 10) ? "0" + s:s
     hours.innerHTML = h;
     minutes.innerHTML = m;
     seconds.innerHTML = s;
-    ampm.innerHTML = am
+    
 }
 
 var interval = setInterval(clock, 1000);
